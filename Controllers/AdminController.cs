f5e5ca6ea94077ec75544e87ae0d21fd4d6a1771
= <=>
2025-12-18 09:47:52 +0500

    updated 18-12-2025

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Project.Models;
 
@@ -114,6 +115,78 @@ namespace Project.Controllers
             dbcontext.SaveChanges();
             return View();
         }
+        public IActionResult ProdList()
+        {
+            var datashow = dbcontext.Products.Include(x => x.Category).ToList();
+            return View(datashow);
+        }
+
+        public IActionResult DeleteProd(int id )
+        {
+
+            var deletetion = dbcontext.Products.FirstOrDefault(x => x.Id == id); //1044 ==1044
+            if (deletetion != null)
+            {
+                dbcontext.Products.Remove(deletetion);
+                dbcontext.SaveChanges();
+            }
+            return RedirectToAction("ProdList");
+        }
+        [HttpGet]
+        public IActionResult EditProd(Product p ,int id)
+        {
+            var ProdExisting = dbcontext.Products.FirstOrDefault(x => x.Id == id);
+            ViewBag.CategoryList = new SelectList(dbcontext.Categories, "Id", "CateName");
+            if(ProdExisting == null)
+            {
+                NotFound();
+            }
+            return View(ProdExisting);
+
+        }
+        [HttpPost]
+        public IActionResult EditProd(Product p,IFormFile ImgFile)
+        {
+            var existProd = dbcontext.Products.Find(p.Id);
+            if (existProd == null)
+            {
+                NotFound();
+            }
+            else
+            {
+                if(ImgFile != null)
+                {
+                    //step 1 Get the image name like product8.jpg
+                    var imageName = Path.GetFileName(ImgFile.FileName);
+                    //step 2 create folder path where the image  will be stored e.g www.root/Image/
+                    var ImagePath = Path.Combine(HttpContext.Request.PathBase.Value, "wwwroot/Image/");
+                    //step 3 Combine folder Path +image
+                    string imageValue = Path.Combine(ImagePath, imageName);
+                    //step 4 Save the image into folder// Move the image from the folder
+                    using (var stream = new FileStream(imageValue, FileMode.Create))
+                    {
+                        ImgFile.CopyTo(stream);
+                    }
+                    var dbimage = Path.Combine("/Image/", imageName);
+                    existProd.Image = dbimage; //Image/Product8.jpg
+                }
+                existProd.ProductName = p.ProductName;
+                existProd.Description = p.Description;
+                existProd.Price = p.Price;
+                existProd.CategoryId = p.CategoryId;
+                existProd.Quantity = p.Quantity;
+
+                dbcontext.Products.Update(existProd);
+                dbcontext.SaveChanges();
+            }
+            
+            return RedirectToAction("ProdList");
+
+        }
+
+
+
+
 
         //private static List<Student> students = new List<Student>();
         //public IActionResult CreateStud()
